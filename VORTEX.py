db11071f30dd7195d2bbe1a6f7248327141c1108
[ByteCoderK] <[asherapream5]>
2025-05-17 17:15:12 +0530

    Listner.py module - Bug Resolved

--- VORTEX.py
+++ VORTEX.py
@@ -4,54 +4,46 @@ import sys
 project_root = os.path.abspath("C:\\Users\\User One\\Desktop\\VORTEX-v2")
 sys.path.append(project_root)
 
-#commands imports
-from commands.greet import *
-from commands.date import *
-from commands.time import *
-from commands.Weather import *
-from commands.Music import *
+# commands
+from commands.greet import greet
+from commands.date import date
+from commands.time import current_time
+from commands.Weather import live_weather
+from commands.Music import play_music
 
-#core imports
+# core
 from core.NetworkCheckerProtocol import *
 from core.Voice import *
+from core.Listener import *
 
 ConnectionProtocol()
 
 def logic():
     speak(greet())
-    global query,queryList
+
     while True:
-        #listen()
+        listen()
+        query, queryList = listen()
+        if not query:  
+            continue
+
         query = query.lower()
-        
+
         if "time" in queryList:
             speak(current_time())
+
         elif "date" in queryList:
             speak(date())
 
-        #elif any(sentance in query for sentance in ['what is the weather',"what's the weather"]):
-         #   try:
-          #      speak("Please specify the city: ")
-           #     city = input('| USER  :  ')
-            #    live_weather(city)
-            #except Exception as e:
-             #   speakOnly("Error Device Offline.")
-              #  print_colored("| VORTEX : Error Device Offline.",Color.RED)
-
-
         elif any(word in query.lower() for word in ['stop', 'exit', 'quit', 'close', 'deactivate']):
-            speak("GoodBye Master..")
-            print_colored("\r======================== |      DEACTIVATED     | ======================\n",Color.RED)
+            speak("Goodbye Master.")
+            print_colored("\n======================== | DEACTIVATED | ======================", Color.RED)
             sys.exit()
 
         elif query.strip() == "":
-            print_colored("| VORTEX : I'm sorry, Master. I didn't understand that. Could you please repeat or try a different command?", Color.YELLOW)
-            print(query)
+            print_colored("| VORTEX : I'm sorry, Master. I didn't understand that. Could you please repeat?", Color.YELLOW)
         else:
-            print_colored("| VORTEX :  PASS-BY ", Color.RED)
-            pass
+            print_colored("| VORTEX : PASS-BY", Color.RED)
+
 if __name__ == '__main__':
-    logic()
-else: 
-    print("WARNING : Running as imported Module")
     logic()

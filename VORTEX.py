35210e35dc2d8fa6577e1954b386d80eab1b30d3
Asher <asherapream5@gmail.com>
2025-11-25 20:38:33 +0530

    Update VORTEX.py updated to return raw json in string

--- VORTEX.py
+++ VORTEX.py
@@ -1,20 +1,18 @@
 import requests
 
-
 def logic():
     while True:
         query = input("You: ")
         res = requests.post("https://vortex-v2.onrender.com/ask", json={"query": query})
-        #print("RAW:", res.text)  
         data = res.json()
-        #print("DEBUG:", data)  # Add this line to see raw response
-        
-        if "response" in data:
-            api_res = "VORTEX:", data["response"]
-            return {"response": data["response"]}
-        else:
-            print("VORTEX: Error",data.get("details","Unknown error"))        
 
+        if "response" in data:
+            # Force response to be a string
+            response_str = str(data["response"])
+            print("VORTEX:", response_str)  # Optional console log
+            return {"response": response_str}  # This is proper JSON for browsers
+        else:
+            print("VORTEX: Error", data.get("details", "Unknown error"))
 
 if __name__ == '__main__':
     logic()

f1d13bfc8cd1791d5b2ccd66d233453967af88e3
Asher <asherapream5@gmail.com>
2025-11-27 21:34:35 +0530

    Update vortex_api.py Debug Trial #10 for XAutomation

--- vortex_api.py
+++ vortex_api.py
@@ -25,6 +25,7 @@ def ask(payload: Query):
     # Handle None safely and log a simple warning
     if RawOutput is None:
         q=payload.query
+        print(RawOutput)
         print (f"route_command returned None for query={q}")
         FinalOutput = ""
         return {"ATLAS": FinalOutput}

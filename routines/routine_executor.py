f669d6c08f60a9cafd289b8a4f66b9ea9e424f0e
Asher <asherapream5@gmail.com>
2025-12-03 21:45:28 +0530

    Update routine_executor.py

--- routines/routine_executor.py
+++ routines/routine_executor.py
@@ -1,7 +1,13 @@
 #routine_executor.py
+import time
+import os
+import sys
+
+project_root = os.path.abspath("C:\\Users\\User One\\Desktop\\VORTEX-v2")
+sys.path.append(project_root)
 
 from commands.XAUTOMATION import *
-import time
+
 
 def execute_action(action):
     """
